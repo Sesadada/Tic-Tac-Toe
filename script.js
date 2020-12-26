4b1c18642a922aa6f8c02a278734e026cfdaee88
Sesadada <serenaolivieri@riseup.net>
2020-12-26 19:23:25 +0100

    starting AI

--- script.js
+++ script.js
@@ -1,58 +1,33 @@
-
+//Players factory -----------------------------------------
 const Player = (name, marker) => {
-  const message = document.querySelector(".message");
-  let privatePoints = [];
-  this.cell;
-  let self = this;
-  const board = document.querySelector(".game--container");
-  const getPlay = () => {
-    board.addEventListener("click", (e) => {
-      e.preventDefault();
-      if (e.target.textContent == "") {
-        e.target.textContent = marker;
-        self.cell = parseInt(e.target.getAttribute("data-cell-index"));
-        privatePoints.push(self.cell);
-        if (privatePoints.length >= 3) {
-          const privateSorted = privatePoints.slice().sort();
-          console.log(privateSorted);
-          for (const x of gameBoard.winCon) {
-            if (
-              x
-                .slice()
-                .sort()
-                .every((value) => privateSorted.includes(parseInt(value)))
-            ) {
-              play.update.textContent = `${name} wins!`;
-              console.log(`${name} wins!`);
-
-              //setTimeout(window.location.reload.bind(window.location), 3000);
-            }
-          }
-        }
-      }
-    });
-    return self.cell;
-  };
-
   const getMarker = () => marker;
   const getName = () => name;
-  const getPoints = () => privatePoints;
 
   return {
     getMarker,
     getName,
-    getPlay,
-    getPoints,
+
   };
 };
 
-//gameBoard render ----------------------------------------------------------------
-const gameBoard = (() => {
-  let points = ["", "", "", "", "", "", "", "", ""]; //create empty array waiting for marks
-  const cells = document.querySelectorAll(".cell"); //selects all spaces in the dom ready to be marked from array
+//Gameboard module ----------------------------------------
+const gameBoard = (() =>{
+let points = ["", "", "", "", "", "", "", "", ""]; 
+const cells = document.querySelectorAll(".cell"); 
 
-  points = points.map((point, index) => {
-    //renders the Point array to the DOM
+const winCon = [
+  ["0", "1", "2"],
+  ["3", "4", "5"],
+  ["6", "7", "8"],
+  ["0", "3", "6"],
+  ["1", "4", "7"],
+  ["2", "5", "8"],
+  ["0", "4", "8"],
+  ["2", "4", "6"],
+];
+
+const rendering = () => {
+  let newBoard = points.map((point, index) => {
     cells.forEach((cell) => {
       let idx = parseInt(cell.getAttribute("data-cell-index"));
       if (index == idx) {
@@ -60,64 +35,123 @@ const gameBoard = (() => {
       }
     });
   });
+  return newBoard
+}
+return {rendering, winCon, points}
+})()
 
-  const displayTurn = (player) => {
-    //play turn function (select cell from DOM)
-    if (player.getPlay()) {
-      points = points.map(x, (index) => {
-        if (index == player.getPlay()) {
-          x = player.getMarker;
-        }
-      });
+//playGame module-----------------------------------------
+const playGame = (()=> { 
+const ai = document.querySelector(".game--ai")
+const board = document.querySelector(".game--container");
+const update = document.querySelector(".update");
+const start = document.querySelector("#start");
+let namePlayerX = document.getElementById("playerX");
+let namePlayer0 = document.getElementById("player0");
+let started = "";
+let won = "no"
+let playerX;
+let player0;
+let currentPlayer;
+let xPoints = []
+let oPoints = []
+let aiplay = "no"
+
+const zero = () => {
+  location.reload();
+  namePlayer0.value = ""
+  namePlayerX.value = ""
+  started = ""
+}
+
+ai.addEventListener("click", (e) => {
+  e.preventDefault()
+ 
+  if(started != ""){
+    update.textContent = `You need to restart the AI to play`
+    zero()
+  } else {
+    aiplay = "yes"
+    started = 1
+    gameInit("Human", "Computer")
+  }
+  
+
+})
+
+start.addEventListener("click", (e) => {
+  e.preventDefault()
+  if (started == "" && namePlayerX.value != "" && namePlayer0.value != "") {
+    gameInit(namePlayerX.value,namePlayer0.value)
+    started = 1
+  } else {
+    zero()
+    gameInit(namePlayerX.value,namePlayer0.value)
+    started = 1
     }
-    return points;
+});
+
+const gameInit = (name1, name2) => {
+    playerX = Player(name1, "X");
+    player0 = Player(name2, "0");
+    Math.round(Math.random()) == 0
+      ? (currentPlayer = playerX)
+      : (currentPlayer = player0);
+    if(aiplay == "yes"){
+console.log("yes")
+    } else {
+    gameRound(currentPlayer);
+    update.textContent = `${currentPlayer.getName()} starts!`;
+    console.log(`currentplayer in gameinit: ${currentPlayer.getName()}`)
+    }
   };
 
-  const winCon = [
-    ["0", "1", "2"],
-    ["3", "4", "5"],
-    ["6", "7", "8"],
-    ["0", "3", "6"],
-    ["1", "4", "7"],
-    ["2", "5", "8"],
-    ["0", "4", "8"],
-    ["2", "4", "6"],
-  ];
-
-  return { displayTurn, winCon };
-})();
-
-const play = (() => {
-  let playerX;
-  let player0;
-  let currentPlayer;
-  const update = document.querySelector(".update");
-  const start = document.querySelector("#start");
-
-  const playGame = (player, x, o) => {
-    player = player === x ? o : x;
-    gameBoard.displayTurn(player);
-    update.textContent = `${player.getName()} playing`;
+const winnerChecker = (arr, player) =>{
+  if (arr.length >= 3) {
+    const privateSorted =  arr.slice().sort();
+    for (const x of gameBoard.winCon) {
+      if (
+        x.slice().sort().every((value) => privateSorted.includes(value))
+      ) {
+        update.textContent = `${player.getName()} wins!`;
+        won = "yes"
+      }
+    }
+  } else if(xPoints.length + oPoints.length == 9 && won != "yes"){
+    update.textContent = `It's a tie!`
+  }
+}
+  
+const gameRound = () => {
+  currentPlayer = currentPlayer === playerX ? player0 : playerX;
   };
 
-  let started = "";
-
-  start.addEventListener("click", (e) => {
-    let namePlayerX = document.getElementById("playerX").value;
-    let namePlayer0 = document.getElementById("player0").value;
-    if (started == 1) {
-      location.reload();
-    } else if (update.textContent != "") {
-      started = 1;
-      playerX = Player(namePlayerX, "X");
-      player0 = Player(namePlayer0, "0");
-      Math.round(Math.random()) == 0
-        ? (currentPlayer = playerX)
-        : (currentPlayer = player0);
-      playGame(currentPlayer, playerX, player0);
+board.addEventListener("click", (e) =>{
+  e.preventDefault()
+  if (started == "" || started == "" && aiplay == "yes"){
+    update.textContent = `Either you didn't put the names or press start!`
+   
+  } else {
+  if(won === "yes"){
+    zero()
+  } else {
+  let idx = e.target.getAttribute("data-cell-index")
+  if(gameBoard.points[idx] === "") {
+    gameBoard.points.splice(idx, 1, currentPlayer.getMarker())
+    e.target.textContent = currentPlayer.getMarker()
+    update.textContent = `${currentPlayer.getName()} is playing`;
+    if(currentPlayer.getMarker() == "X"){
+      xPoints.push(idx)
+      winnerChecker(xPoints, currentPlayer)
+    } else {
+      oPoints.push(idx)
+      winnerChecker(oPoints, currentPlayer)
     }
-  });
-
-  return { update };
-})();
-
+    gameBoard.rendering()
+    gameRound()
+  } else {
+    update.textContent = `This spot is already taken`;
+  }}
+} 
+})
+})()
